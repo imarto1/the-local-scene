6cb51218164ea84a33d830fa2e87f4d8abbc4a82
itamar <itamar.marton@gmail.com>
2025-04-23 21:22:46 +0300

    enabled cors, fixed bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,22 @@
 import './App.css'
 import { MapContainer, TileLayer, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import { useState, useEffect } from 'react';
-import * as icons from "lucide-react"
-import MapMarker from './components/marker/marker';
+import { QueryClient,  QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import Map from './components/map/map';
+
 
 function App() {
-  const [userLocation, setUserLocation] = useState(null);
-  const []
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          setUserLocation([latitude, longitude]);
-        },
-        (error) => {
-          setUserLocation([51.505, -0.09]); // Default location
-          // alert("Unable to fetch your location. Please enable location services.");
-          console.error("Error fetching location:", error);
-        },
-        { enableHighAccuracy: true, timeout: 10000, maximumAge: 0 } // Mobile-friendly options
-      );
-    } else {
-      console.error("Geolocation is not supported by this browser.");
-    }
-  }, [userLocation]);
-  return <>
-  {userLocation ? <div id="root" style={{ height: "100vh", width: "100%" }}>
-      <MapContainer center={userLocation} zoom={17} style={{ height: "100%", width: "100%" }}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <MapMarker position={userLocation} icon={<icons.PersonStandingIcon color='green' size={32}/>}>
-          <Popup>
-            You are here!
-          </Popup>
-        </MapMarker>
-      </MapContainer>
-    </div> : <h1>"Couldn't get location"</h1>}
-  </>
-    
+  const queryClient = new QueryClient();
+  return <div id="root" style={{ height: "100vh", width: "100%" }}>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools/>
+      <Map/>
+    </QueryClientProvider>
+    </div>
+ 
+
 }
 
-export default App
+export default App  
 
--- server/main.py
+++ server/main.py
@@ -1,21 +1,51 @@
+import os
+
+from elasticsearch import Elasticsearch
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from models.event import Event, GetEventsBody, GetEventsResponse
-import os
-from elasticsearch import Elasticsearch
-
 
 client = Elasticsearch(
-    hosts=["https://localhost:32771"],
+    hosts=["https://localhost:32768/"],
     api_key=os.getenv("ELASTIC_API_KEY", "ZVJJRldKWUJHbi1mVG5feHlMWks6blFfdzBKYUZSYWdwSjVTelRnN2QxQQ"),
     verify_certs=False,
 )
 app = FastAPI()
 
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace "*" with specific origins if needed
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+def fetch_events(lon, lat, distance):
+    return client.search(
+        index="events",
+        query={
+            "bool": {
+                "filter": {
+                    "geo_distance": {
+                        "distance": f"{distance}km",
+                        "location": {
+                            "lon": lon,
+                            "lat": lat
+                        }
+                    }
+                }
+            }
+        }
+    )["hits"]["hits"]
+
 
 @app.get("/events", response_model=GetEventsResponse)
-def get_events(body: GetEventsBody):
-    return GetEventsResponse(events=[])
+def get_events(lon: float, lat: float, radius: int):
+    events = fetch_events(lon, lat, radius)
+    return GetEventsResponse(events=[{"id": event["_id"], **event["_source"]} for event in events])
 
 
 @app.post("/event")
